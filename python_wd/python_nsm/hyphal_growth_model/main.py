b6c9d70bc547390d33818b02ae103fd33b26af44
Edoardo Tancredi <124071261+ETancredi@users.noreply.github.com>
2025-06-04 14:23:56 +0100

    Update main.py

--- python_wd/python_nsm/hyphal_growth_model/main.py
+++ python_wd/python_nsm/hyphal_growth_model/main.py
@@ -190,7 +190,7 @@ def generate_outputs(mycel, components, output_dir="outputs"):
     )
 
     export_tip_history(mycel, f"{output_dir}/mycelium_time_series.csv")
-    export_biomass_history(mycel, f"{output_dir}/biomass_history.csv")
+    export_biomass_history(mycel, f"{output_dir}/biomass_and_tips_history.csv")
 
 def simulate(opts, steps=120):
     mycel, components = setup_simulation(opts)
